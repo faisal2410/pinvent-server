a6a7bfac533826871f8798ebe74d1c8b5f84b013
Faisal ahmed <faisal2410@yahoo.com>
2022-12-30 17:44:36 +0600

    product crud

--- routes/product.js
+++ routes/product.js
@@ -1,11 +1,21 @@
 const express = require("express");
 const router = express.Router();
 const protect = require("../middleWares/auth");
-const {createProduct} = require("../controllers/product");
+const {createProduct,
+    getProducts,
+    getProduct,
+    deleteProduct,
+    updateProduct
+
+} = require("../controllers/product");
 const { upload } = require("../utils/fileUpload");
 
 
 router.post("/", protect,upload.single("image"),createProduct);
+router.patch("/:id", protect, upload.single("image"), updateProduct);
+router.get("/", protect, getProducts);
+router.get("/:id", protect, getProduct);
+router.delete("/:id", protect, deleteProduct);
 
 
 module.exports = router;
